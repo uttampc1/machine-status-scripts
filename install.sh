0db6d0eeadeb09588b9717f995ed4f087122829d
Uttam Pawar <upawar@amd.com>
2026-03-26 03:05:23 +0000

    added simple reporting for fun!

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 sudo echo "Installing template and creating symlinks in /usr/local directory"
-if [ -r /etc/motd ]; then
-  echo "File is readable"
-  if [ -w /etc/motd ]; then
-    echo "File is readable"
-  fi
-else
-  sudo touch /etc/motd
+if [ ! -f /etc/motd ]; then
+  echo
+  echo "It seems /etc/motd file is not a regular file."
+  echo "Please make sure we can create it as a regular file"
+  echo
+  exit
 fi
+
+sudo touch /var/log/machine-status.log
+sudo chmod 666 /var/log/machine-status.log
+sudo touch /etc/motd
 sudo chmod 666 /etc/motd
+
+sudo /bin/rm /usr/local/bin/machine-status /usr/local/bin/machine-release /usr/local/bin/machine-reserve /usr/local/bin/machine-report
 sudo cp ./machine-status /usr/local/bin
+sudo cp ./motd.template /usr/local/etc
 sudo chmod 555 ./machine-status /usr/local/bin
 sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-release
 sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-reserve
+sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-report
 machine-release
