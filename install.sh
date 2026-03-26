13669e366090fafae85209470796f254c39f1f5f
Uttam Pawar <upawar@amd.com>
2026-03-26 03:51:17 +0000

    simplified install step

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 sudo echo "Installing script, template and creating symlinks in /usr/local directory"
-if [ ! -f /etc/motd ]; then
-  echo
-  echo "It seems /etc/motd file is not a regular file."
-  echo "Please make sure we can create it as a regular file"
-  echo
-  exit
-fi
 
-sudo touch /var/log/machine-status.log
-sudo chmod 666 /var/log/machine-status.log
 sudo touch /etc/motd
 sudo chmod 666 /etc/motd
 
@@ -32,4 +23,6 @@ sudo chmod 555 ./machine-status /usr/local/bin
 sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-release
 sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-reserve
 sudo ln -s /usr/local/bin/machine-status /usr/local/bin/machine-report
+sudo touch /var/log/machine-status.log
+sudo chmod 666 /var/log/machine-status.log
 machine-release
