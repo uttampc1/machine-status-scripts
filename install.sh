d6a06cf6c22a5ce51a070dd37aebd4e791103e6b
Uttam Pawar <upawar@amd.com>
2026-03-26 17:39:20 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo echo "Installing script, template and creating symlinks in /usr/local direc
 TARGET_SCRIPT="/usr/local/bin/machine-status"
 MOTD_TEMPLATE="/usr/local/etc/motd.template"
 
+sudo touch /var/log/machine-status.log
+sudo chmod 666 /var/log/machine-status.log
+
 copy_it=0
 if [ ! -f ${TARGET_SCRIPT} ]; then
   copy_it=1
@@ -28,19 +31,6 @@ else
   fi
 fi
 
-if [ ${copy_it} -eq 1 ]; then
-  sudo cp ./motd.template ${MOTD_TEMPLATE}
-fi
-
-if [ -f /etc/motd ]; then
-  sudo cp /etc/motd /etc/motd.orig
-  echo "Saved existing /etc/motd file as /etc/motd.orig"
-else
-  sudo touch /etc/motd
-fi
-
-sudo chmod 666 /etc/motd
-
 if [ ! -L /usr/local/bin/machine-reserve ]; then
   sudo ln -s ${TARGET_SCRIPT} /usr/local/bin/machine-reserve
 fi
@@ -53,7 +43,20 @@ if [ ! -L /usr/local/bin/machine-report ]; then
   sudo ln -s ${TARGET_SCRIPT} /usr/local/bin/machine-report
 fi
 
-sudo touch /var/log/machine-status.log
-sudo chmod 666 /var/log/machine-status.log
+if [ ${copy_it} -eq 1 ]; then
+  sudo cp ./motd.template ${MOTD_TEMPLATE}
+fi
 
-machine-status
+sudo touch /etc/motd
+if [ -f "/etc/motd" ] && [ ! -s "/etc/motd" ]; then
+	# File exists and is empty
+  machine-release
+elif [ ! -e "/etc/motd" ]; then
+	# File does not exists
+  machine-release
+else
+	# File exists and is not empty, show content
+  machine-status
+fi
+sudo chmod 666 /etc/motd
+
